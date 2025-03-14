5868c5700f70974e157e05fa0554ffc9f2dc7c22
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-03-15 04:30:40 +0600

    add main section

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log('Hello, world!');
+// console.log('Hello, world!');
+
+//function for load category-btn-container
+function loadCategoryBtnContainer() {
+    // fetch data from api
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        // convirt data to json()
+        .then(res => res.json())
+        // send data to enother function to display
+        .then((data) => displayCategoryBtn(data.categories))
+        
+}
+
+//function for display category-btn-container
+function displayCategoryBtn(categories) { 
+    // get the container to display category
+    const categoryContainer = document.getElementById('category-btn-container');
+    // loop for each data
+    for (let cat of categories) {
+        // create element
+        const categoryDiv = document.createElement('div');
+        // set innerText
+        categoryDiv.innerHTML = `
+            <button class="btn btn-active hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        `
+        // append to parent
+        categoryContainer.appendChild(categoryDiv);
+    }
+}
+
+
+
+
+loadCategoryBtnContainer();
+// const loadBtn = document.getElementById('category-btn-container');
